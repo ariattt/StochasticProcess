d3c0fa6f59d78e50d8492c5367c6407df50cc07d
Bazinga <Bazinga@wireless-10-148-25-79.public.utexas.edu>
2019-12-02 23:29:04 -0600

    HW5

---
+++ DoctorWho.py
@@ -0,0 +1,31 @@
+import markov362m as mk
+
+madRat = mk.MarkovChain(title = "Crazy Dumb Rat")
+madRat.add_state("1")
+madRat.add_state("2")
+madRat.add_state("3")
+madRat.add_state("4")
+madRat.add_state("5")
+madRat.add_state("S")
+madRat.add_state("F")
+
+madRat.add_transition("1","2",1.0/2)
+madRat.add_transition("1","3",1.0/2)
+madRat.add_transition("3","1",1.0/3)
+madRat.add_transition("3","4",1.0/3)
+madRat.add_transition("3","S",1.0/3)
+madRat.add_transition("2","1",1.0/3)
+madRat.add_transition("2","4",1.0/3)
+madRat.add_transition("2","F",1.0/3)
+madRat.add_transition("4","2",1.0/3)
+madRat.add_transition("4","3",1.0/3)
+madRat.add_transition("4","5",1.0/3)
+madRat.add_transition("5","4",1.0/2)
+madRat.add_transition("5","F",1.0/2)
+madRat.add_transition("S","S",1.0)
+madRat.add_transition("F","F",1.0)
+
+madRat.compute()
+print(madRat.info_P())
+print(madRat.F @ madRat.R)
+
